a492298671ed67f2dd1decd8a083c50ffe043e98
Martin <martin.salvachua1@gmail.com>
2019-06-27 15:07:03 +0200

    Probando cosas en el mapa

--- FiwareMap/views.py
+++ FiwareMap/views.py
@@ -19,7 +19,7 @@ path_token = '/v3/auth/tokens'
 # url para pedir todas las entidades
 puerto_entidades = ':2026'
 path_entidades = '/v2/entities?type=Arbol'
-opciones_entidades = '&limit=1&options=count'
+opciones_entidades = '&limit=1000&options=count'
 
 
 def obtenerToken():
